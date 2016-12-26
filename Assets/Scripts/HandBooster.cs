31392fabd120ef681dbc6589f69ceafd201d20d3
tallguy9271 <drewoken@comcast.net>
2016-12-26 14:57:42 -0500

    - thumbpad lets you change power of boosters - text displays power of your boosters

--- Assets/Scripts/HandBooster.cs
+++ Assets/Scripts/HandBooster.cs
@@ -6,9 +6,12 @@ public class HandBooster : MonoBehaviour {
 
     [SerializeField]
     private float boosterForce = 1.0f;
+    [SerializeField]
+    private float accelerationSpeed = 1.0f;
     private bool boosting = false;
 
-    public Rigidbody CameraRigRigidbody;    
+    public Rigidbody CameraRigRigidbody;
+    public TextMesh BoostPowerText;    
 
     private SteamVR_TrackedObject trackedObj;
     private SteamVR_Controller.Device Controller
@@ -19,11 +22,21 @@ public class HandBooster : MonoBehaviour {
     void Awake()
     {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
+        if (BoostPowerText) BoostPowerText.text = boosterForce.ToString();
     }
 
     void Update()
     {
 
+        if (Controller.GetAxis() != Vector2.zero)
+        {
+            Debug.Log(gameObject.name + Controller.GetAxis());
+            float y = Controller.GetAxis().y;
+            if (y > 0) boosterForce += accelerationSpeed;
+            else boosterForce -= accelerationSpeed;
+            if (BoostPowerText) BoostPowerText.text = boosterForce.ToString();
+        }
+        
         if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
         {
             Debug.Log(gameObject.name + " Grip Down");
@@ -39,7 +52,6 @@ public class HandBooster : MonoBehaviour {
         if (boosting)
         {
             CameraRigRigidbody.AddForce(transform.forward * boosterForce * Time.deltaTime);
-            Debug.Log(transform.forward * boosterForce * Time.deltaTime);
         }
 
     }
